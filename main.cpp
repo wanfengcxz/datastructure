4c400aaef3981942c107981849f69326c1ce77b5
git <2917021186@qq.com>
2021-05-04 21:18:36 +0800

    add:哈希表存储稀疏矩阵？

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include "gtest/gtest.h"
 #include <stack>
+#include <unordered_map>
+
+using namespace std;
 
 class stack_test : public ::testing::Test {
 protected:
@@ -17,29 +20,83 @@ protected:
 
 };
 
+TEST_F(stack_test, demo) {
+    ASSERT_EQ(teststack.top(), 2);
+}
+
 int add(int a, int b) {
     return a + b;
 }
 
 TEST(add, zero) {
-EXPECT_EQ(0, add(0, 0)) << "yes";
+    EXPECT_EQ(0, add(0, 0)) << "yes";
 }
 
 TEST(add, positive_number) {
-EXPECT_EQ(3, add(1, 2));
+    EXPECT_EQ(3, add(1, 2));
 }
 
 TEST(add, negative_number) {
-EXPECT_EQ(-3, add(-1, -2));
+    EXPECT_EQ(-3, add(-1, -2));
 }
+
+struct Index2 {
+    int x, y;
+
+    explicit Index2(int x = 0, int y = 0) : x(x), y(y) {}
+};
+
+struct Index2Hash {
+
+    size_t operator()(const Index2 &index) const {
+        // hash<int>()(...) 仿函数？
+        return hash<int>()(index.x) ^ hash<int>()(index.y);
+    }
+
+};
+
+struct Index2Com {
+
+    bool operator()(const Index2 &index1, const Index2 &index2) const {
+        return index1.x == index2.x && index1.y == index2.y;
+    }
+};
+
+class SparseMatrix {
+private:
+    unordered_map<Index2, int, Index2Hash, Index2Com> map;
+    int count;
+public:
+    SparseMatrix() : count(0) {}
+
+    int at(int x, int y) {
+        if (map.count(Index2(x, y)) <= 0) {
+            return 0;
+        } else {
+            return map[Index2(x, y)];
+        }
+    }
+
+    void insert(int x, int y, int e) {
+//        map[Index2(x,y)] = e;
+        map.emplace(Index2(x,y), e);
+    }
+
+};
 
 int main() {
 //    int a = 5,b = 1;
 //    std::cout << (b=a) << std::endl;
-    std::stack<int> s;
-    std::cout << s.top() << std::endl;
-    ::testing::InitGoogleTest();
-    return RUN_ALL_TESTS();
+
+//    std::stack<int> s;
+//    std::cout << s.top() << std::endl;
+
+//    ::testing::InitGoogleTest();
+//    return RUN_ALL_TESTS();
+
+
+
+    return 0;
 }
 
 
--- problem.md
+++ problem.md
@@ -1,3 +1,4 @@
 P66 二.5 Stack/ShareStack<br>
-P80 二.3 Stack/Problem/<br>
+P80 二.3 Stack/Problem<br>
 P90 二.1 Stack/Application/BracketMatching<br>
+P90 二.2 Stack/Problem<br>
